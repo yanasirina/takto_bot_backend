ddcdaddc975da7705e9825a1b02609533a04452f
Areacas <zaxarrovdima@ya.ru>
2023-12-11 23:53:31 +0300

    админка

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from core import models
+
+
+@admin.register(models.Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['telegram_id', 'username', 'name', 'phone_number']
+    search_fields = ['telegram_id', 'username', 'name', 'phone_number']
+
