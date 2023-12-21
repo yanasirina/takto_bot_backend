d0bc27a85a1b50fe6842ff506b3f41236bb0cadb
Areacas <zaxarrovdima@ya.ru>
2023-12-21 23:35:23 +0300

    #8 преобразовал модель User из ReadOnlyModelViewSet в ModelViewSet + переопределил сериализаторы

--- core/views/user.py
+++ core/views/user.py
@@ -6,10 +6,19 @@ from core.permissions import DjangoModelPermissions
 from core.views.pagination import RestPagination
 
 
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication, SessionAuthentication)
     permission_classes = (DjangoModelPermissions,)
     queryset = models.DjangoUser.objects.all().order_by('last_name', 'first_name', 'username')
     pagination_class = RestPagination
     filterset_class = filters.User
     serializer_class = serializers.User
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return serializers.User
+        elif self.action == 'retrieve':
+            return serializers.UserDetailsSerializer
+        elif self.action in ['create', 'update', 'partial_update']:
+            return serializers.UserCreateUpdateSerializer
+        return super().get_serializer_class()
